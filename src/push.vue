61e67cc3a843a2f572d2b7c3398ed816fc9b9a1f
LENOVO <zokir.yorov@mail.ru>
2024-04-01 11:26:02 +0500

    push

---
+++ src/push.vue
@@ -0,0 +1,15 @@
+<template>
+  <form>
+    <div>
+      <h2>login</h2>
+      <input type="text" name="number">
+      <input type="password" name="number">
+    </div>
+  </form>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import pushCode from '@/push.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
+    {
+      path: "/push",
+      name: "push",
+      component: pushCode
+    }
   ]
 })
 export default router
